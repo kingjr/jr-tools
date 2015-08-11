d434364793afa02b354915af7b24cbccdbe102b3
kingjr <jeanremi.king+github@gmail.com>
2015-08-11 07:54:25 -0400

    BUG

--- jr/gat/__init__.py
+++ jr/gat/__init__.py
@@ -1,7 +1,7 @@
 from .base import (equalize_samples, subscore, subselect_ypred, mean_ypred,
-                   rescale_ypred, zscore_ypred, GAT, GATs, combine_y
-from .classifiers import (SSSLinearClassifier, force_predict, force_weight
-		          LinearSVC_Proba, SVC_Light)
+                   rescale_ypred, zscore_ypred, GAT, GATs, combine_y)
+from .classifiers import (SSSLinearClassifier, force_predict, force_weight,
+                          LinearSVC_Proba, SVC_Light)
 from .predicters import predict_OneVsOne, predict_OneVsRest
 from .scalers import MedianScaler, MedianClassScaler, StandardClassScaler
 from .scorers import scorer_spearman, scorer_auc, prob_accuracy
