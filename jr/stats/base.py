a6b132dbb4f8fd60bb05c9ff0de641be6c7c31c0
kingjr <jeanremi.king+github@gmail.com>
2015-08-11 12:56:24 -0400

    EHN: add stats functions

---
+++ jr/stats/base.py
@@ -0,0 +1,145 @@
+import numpy as np
+
+
+def circular_linear_correlation(X, alpha):
+    # Authors:  Jean-Remi King <jeanremi.king@gmail.com>
+    #           Niccolo Pescetelli <niccolo.pescetelli@gmail.com>
+    #
+    # Licence : BSD-simplified
+    """
+
+    Parameters
+    ----------
+        X : numpy.array, shape (n_angles, n_dims)
+            The linear data
+        alpha : numpy.array, shape (n_angles,)
+            The angular data (if n_dims == 1, repeated across all x dimensions)
+    Returns
+    -------
+        R : numpy.array, shape (n_dims)
+            R values
+        R2 : numpy.array, shape (n_dims)
+            R square values
+        p_val : numpy.array, shape (n_dims)
+            P values
+
+    Adapted from:
+        Circular Statistics Toolbox for Matlab
+        By Philipp Berens, 2009
+        berens@tuebingen.mpg.de - www.kyb.mpg.de/~berens/circStat.html
+        Equantion 27.47
+    """
+
+    from scipy.stats import chi2
+    import numpy as np
+
+    # computes correlation for sin and cos separately
+    rxs = repeated_corr(X, np.sin(alpha))
+    rxc = repeated_corr(X, np.cos(alpha))
+    rcs = repeated_corr(np.sin(alpha), np.cos(alpha))
+
+    # tile alpha across multiple dimension without requiring memory
+    if X.ndim > 1 and alpha.ndim == 1:
+        rcs = tile_memory_free(rcs, X.shape[1:])
+
+    # Adapted from equation 27.47
+    R = (rxc ** 2 + rxs ** 2 - 2 * rxc * rxs * rcs) / (1 - rcs ** 2)
+
+    # JR adhoc way of having a sign....
+    R = np.sign(rxs) * np.sign(rxc) * R
+    R2 = np.sqrt(R ** 2)
+
+    # Get degrees of freedom
+    n = len(alpha)
+    pval = 1 - chi2.cdf(n * R2, 2)
+
+    return R, R2, pval
+
+
+def repeated_corr(X, y, dtype=float):
+    """Computes pearson correlations between a vector and a matrix.
+
+    Adapted from Jona-Sassenhagen's PR #L1772 on mne-python.
+
+    Parameters
+    ----------
+        y : np.array, shape (n_samples)
+            Data vector.
+        X : np.array, shape (n_samples, n_measures)
+            Data matrix onto which the vector is correlated.
+        dtype : type, optional
+            Data type used to compute correlation values to optimize memory.
+
+    Returns
+    -------
+        rho : np.array, shape (n_measures)
+    """
+    from sklearn.utils.extmath import fast_dot
+    if X.ndim not in [1, 2] or y.ndim != 1 or X.shape[0] != y.shape[0]:
+        raise ValueError('y must be a vector, and X a matrix with an equal'
+                         'number of rows.')
+    if X.ndim == 1:
+        X = X[:, None]
+    y -= np.array(y.mean(0), dtype=dtype)
+    X -= np.array(X.mean(0), dtype=dtype)
+    y_sd = y.std(0, ddof=1)
+    X_sd = X.std(0, ddof=1)[:, None if y.shape == X.shape else Ellipsis]
+    return (fast_dot(y.T, X) / float(len(y) - 1)) / (y_sd * X_sd)
+
+
+def tile_memory_free(y, shape):
+    """
+    Tile vector along multiple dimension without allocating new memory.
+
+    Parameters
+    ----------
+     y : np.array, shape (n,)
+        data
+    shape : np.array, shape (m),
+    Returns
+    -------
+    Y : np.array, shape (n, *shape)
+    """
+    y = np.lib.stride_tricks.as_strided(y,
+                                        (np.prod(shape), y.size),
+                                        (0, y.itemsize)).T
+    return y.reshape(np.hstack((len(y), shape)))
+
+
+def repeated_spearman(X, y, dtype=None):
+    """Computes spearman correlations between a vector and a matrix.
+
+    Parameters
+    ----------
+        X : np.array, shape (n_samples, n_measures)
+            Data matrix onto which the vector is correlated.
+        y : np.array, shape (n_samples)
+            Data vector.
+        dtype : type, optional
+            Data type used to compute correlation values to optimize memory.
+
+    Returns
+    -------
+        rho : np.array, shape (n_measures)
+    """
+    if X.ndim not in [1, 2] or y.ndim != 1 or X.shape[0] != y.shape[0]:
+        raise ValueError('y must be a vector, and X a matrix with an equal'
+                         'number of rows.')
+    if X.ndim == 1:
+        X = X[:, None]
+
+    # Rank
+    X = np.argsort(X, axis=0)
+    y = np.argsort(y, axis=0)
+    # Double rank to ensure that normalization step of compute_corr
+    # (X -= mean(X)) remains an integer.
+    if (dtype is None and X.shape[0] < 2 ** 8) or\
+       (dtype in [int, np.int16, np.int32, np.int64]):
+        X *= 2
+        y *= 2
+        dtype = np.int16
+    else:
+        dtype = type(y[0])
+    X = np.array(X, dtype=dtype)
+    y = np.array(y, dtype=dtype)
+    return repeated_corr(X, y, dtype=type(y[0]))
---
+++ jr/stats/tests/test_stats.py
@@ -0,0 +1,38 @@
+import numpy as np
+from ..base import repeated_corr, repeated_spearman
+
+
+def test_corr_functions():
+    from scipy.stats import spearmanr
+    test_corr(np.corrcoef, repeated_corr, 1)
+    test_corr(spearmanr, repeated_spearman, 0)
+
+
+def test_corr(old_func, new_func, sel_item):
+    from nose.tools import assert_equal, assert_raises
+    n_obs = 20
+    n_dims = 10
+    y = np.linspace(0, 1, n_obs)
+    X = np.tile(y, [n_dims, 1]).T + np.random.randn(n_obs, n_dims)
+    rho_fast = new_func(X, y)
+    # test dimensionality
+    assert_equal(rho_fast.ndim, 1)
+    assert_equal(rho_fast.shape[0], n_dims)
+    # test data
+    rho_slow = np.ones(n_dims)
+    for dim in range(n_dims):
+        rho_slow[dim] = np.array(old_func(X[:, dim], y)).item(sel_item)
+    np.testing.assert_array_equal(rho_fast.shape, rho_slow.shape)
+    np.testing.assert_array_almost_equal(rho_fast, rho_slow)
+    # test errors
+    new_func(np.squeeze(X[:, 0]), y)
+    assert_raises(ValueError, new_func, y, X)
+    assert_raises(ValueError, new_func, X, y[1:])
+    # test dtype
+    X = np.argsort(X, axis=0) * 2  # ensure no bug at normalization
+    y = np.argsort(y, axis=0) * 2
+    rho_fast = new_func(X, y, dtype=int)
+    rho_slow = np.ones(n_dims)
+    for dim in range(n_dims):
+        rho_slow[dim] = np.array(old_func(X[:, dim], y)).item(sel_item)
+    np.testing.assert_array_almost_equal(rho_fast, rho_slow)
