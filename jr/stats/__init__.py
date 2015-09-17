c02e8740a537e5002be52463833964a4213ff023
kingjr <jeanremi.king+github@gmail.com>
2015-09-17 09:23:09 -0400

    EHN: dprime

--- jr/stats/__init__.py
+++ jr/stats/__init__.py
@@ -1,5 +1,5 @@
 from .base import (corr_linear_circular, repeated_corr, fast_mannwhitneyu,
-                   repeated_spearman, corr_circular, robust_mean)
+                   repeated_spearman, corr_circular, robust_mean, dPrime)
 from .gat_stats import parallel_stats, stats_tfce
 from .circ import (circ_hist, circ_tuning, circ_double_tuning, circ_diff,
                    circ_mean, circ_cummean, circ_std, circ_digitize)
--- jr/stats/base.py
+++ jr/stats/base.py
@@ -267,3 +267,35 @@ def _loop_mannwhitneyu(X, Y, use_continuity=True):
     for ii in range(n_col):
         U[ii], P[ii] = mannwhitneyu(X[:, ii], Y[:, ii], use_continuity)
     return U, P
+
+
+def dPrime(hits, misses, fas, crs):
+    from scipy.stats import norm
+    from math import exp, sqrt
+    Z = norm.ppf
+    # From Jonas Kristoffer Lindelov : lindeloev.net/?p=29
+    # Floors an ceilings are replaced by half hits and half FA's
+    halfHit = 0.5 / (hits + misses)
+    halfFa = 0.5 / (fas + crs)
+
+    # Calculate hitrate and avoid d' infinity
+    hitRate = hits / (hits + misses)
+    if hitRate == 1:
+        hitRate = 1 - halfHit
+    if hitRate == 0:
+        hitRate = halfHit
+
+    # Calculate false alarm rate and avoid d' infinity
+    faRate = fas/(fas+crs)
+    if faRate == 1:
+        faRate = 1 - halfFa
+    if faRate == 0:
+        faRate = halfFa
+
+    # Return d', beta, c and Ad'
+    out = {}
+    out['d'] = Z(hitRate) - Z(faRate)
+    out['beta'] = exp(Z(faRate)**2 - Z(hitRate)**2)/2
+    out['c'] = -(Z(hitRate) + Z(faRate))/2
+    out['Ad'] = norm.cdf(out['d']/sqrt(2))
+    return out
