9e37a31e1e35de2ccfef8ab2fee08a0ad81ad05d
kingjr <jeanremi.king+github@gmail.com>
2015-09-17 10:42:23 -0400

    EHN: plot_gfp

--- jr/plot/meg.py
+++ jr/plot/meg.py
@@ -34,3 +34,33 @@ def plot_butterfly(evoked, ax=None, sig=None, color=None, ch_type=None):
     ax.set_xlim(np.min(times), np.max(times))
     pretty_plot(ax)
     return ax
+
+
+def plot_gfp(evoked, ax=None, sig=None, color=None, ch_type='mag'):
+    from mne import pick_types
+    if ch_type is not None:
+        picks = pick_types(evoked.info, ch_type)
+        evoked = evoked.pick_types(ch_type, copy=True)
+        sig = sig[picks, :] if sig is not None else None
+    times = evoked.times * 1e3
+    gfp = np.std(evoked.data, axis=0)
+    ax = plt.gca() if ax is None else ax
+    ax.plot(times, gfp, color='k', alpha=.5)
+    if sig is not None:
+        sig = np.array(np.sum(sig, axis=0) > 0., dtype=int)
+        ax.fill_between(np.hstack((times, times[::-1])),
+                        np.hstack((sig * gfp, np.zeros_like(gfp))),
+                        facecolor=color, edgecolor='none', alpha=.5)
+    ax.axvline(0, color='k')
+    ax.set_xlabel('Times (ms)')
+    ax.set_xlim(min(times), max(times))
+    xticks = np.arange(np.ceil(min(times)/1e2) * 1e2,
+                       np.floor(max(times)/1e2) * 1e2, 100)
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(['%i' % t if t in [xticks[0], xticks[-1], 0]
+                        else '' for t in xticks])
+    ax.set_yticks([np.min(gfp), np.max(gfp)])
+    ax.set_ylim(np.min(gfp), np.max(gfp))
+    ax.set_xlim(np.min(times), np.max(times))
+    pretty_plot(ax)
+    return ax
