bff20a5d01d3f6f12aa58551abb93f4a13b048c4
kingjr <jeanremi.king+github@gmail.com>
2015-09-17 10:42:35 -0400

    EHN: plot_gfp

--- jr/plot/__init__.py
+++ jr/plot/__init__.py
@@ -4,4 +4,4 @@ from .base import (share_clim, plot_widths, plot_sem, plot_eb,
 from .circ import (plot_sem_polar, pretty_polar_plot, plot_tuning,
                    circ_fill_between)
 from .gat_plot import pretty_gat, pretty_decod, pretty_colorbar, pretty_slices
-from .meg import plot_butterfly
+from .meg import plot_butterfly, plot_gfp
