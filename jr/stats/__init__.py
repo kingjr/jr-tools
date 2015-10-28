49137be0b5beac6a99cc6f64418dab83c5948bb9
kingjr <jeanremi.king+github@gmail.com>
2015-10-28 15:09:04 -0400

    fix

--- jr/stats/__init__.py
+++ jr/stats/__init__.py
@@ -1,6 +1,6 @@
 from .base import (corr_linear_circular, repeated_corr, fast_mannwhitneyu,
                    repeated_spearman, corr_circular, robust_mean, dPrime,
-                   mannwhitneyu, corr_circular_linear)
-from .gat_stats import parallel_stats, stats_tfce, nested_analysis
+                   mannwhitneyu, corr_circular_linear, nested_analysis)
+from .gat_stats import parallel_stats, stats_tfce
 from .circ import (circ_hist, circ_tuning, circ_double_tuning, circ_diff,
                    circ_mean, circ_cummean, circ_std, circ_digitize)
