4f5bcc9891fb84db5af406eb435e05e7b6a524f2
kingjr <jeanremi.king+github@gmail.com>
2016-02-16 15:21:22 -0500

    polar and angular regression in init

--- jr/gat/__init__.py
+++ jr/gat/__init__.py
@@ -2,7 +2,8 @@ from .base import (equalize_samples, subscore, subselect_ypred, mean_ypred,
                    rescale_ypred, zscore_ypred, GAT, GATs, combine_y, MetaGAT,
                    update_pred, get_diagonal_ypred, get_diagonal_score)
 from .classifiers import (SSSLinearClassifier, force_predict, force_weight,
-                          LinearSVC_Proba, SVC_Light, SVR_angle, SVR_polar)
+                          LinearSVC_Proba, SVC_Light, SVR_angle, SVR_polar,
+                          AngularRegression, PolarRegression)
 from .predicters import predict_OneVsOne, predict_OneVsRest
 from .scalers import MedianScaler, MedianClassScaler, StandardClassScaler
 from .scorers import scorer_spearman, scorer_auc, prob_accuracy, scorer_angle
