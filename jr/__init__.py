6a7c0e59b2569fad7254a123a9fca564f10d7430
kingjr <jeanremi.king+github@gmail.com>
2016-02-16 20:38:29 -0500

    FIX: root import

--- jr/__init__.py
+++ jr/__init__.py
@@ -1,5 +1,3 @@
 #   . import *
 __version__ = '0.1.dev0'
-from utils import (pi_labels, tile_memory_free, OnlineReport, nandigitize,
-                   count, product_matrix_vector, logcenter, pairwise,
-                   align_on_diag, table2html)
+from . import *
