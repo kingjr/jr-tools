e3ae171cd49c7677e1d1ad098a9d39eb93225b50
kingjr <jeanremi.king+github@gmail.com>
2016-02-24 13:02:59 -0500

    ENH: cross_correlation with maxlag

--- jr/stats/base.py
+++ jr/stats/base.py
@@ -531,3 +531,34 @@ def align_signals(a, b):
     if len(a) > len(b) and len(a) % 2 and not(len(b) % 2):
         delay += 1
     return sign * delay
+
+
+def cross_correlation(x, y, maxlag):
+    """
+    Cross correlation with a maximum number of lags.
+
+    `x` and `y` must be one-dimensional numpy arrays with the same length.
+
+    This computes the same result as
+        numpy.correlate(x, y, mode='full')[len(a)-maxlag-1:len(a)+maxlag]
+
+    The return vaue has length 2*maxlag + 1.
+
+    Author: http://stackoverflow.com/questions/30677241
+            Warren Weckesser
+    """
+    from numpy.lib.stride_tricks import as_strided
+
+    def _check_arg(x, xname):
+        x = np.asarray(x)
+        if x.ndim != 1:
+            raise ValueError('%s must be one-dimensional.' % xname)
+        return x
+
+    x = _check_arg(x, 'x')
+    y = _check_arg(y, 'y')
+    py = np.pad(y.conj(), 2*maxlag, mode='constant')
+    T = as_strided(py[2*maxlag:], shape=(2*maxlag+1, len(y) + 2*maxlag),
+                   strides=(-py.strides[0], py.strides[0]))
+    px = np.pad(x, maxlag, mode='constant')
+    return T.dot(px)
