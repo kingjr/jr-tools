FIX: memory load robust average

jr/stats/base.py
@@ -260,8 +260,8 @@ def robust_mean(X, axis=None, percentile=[5, 95]):
         X = np.transpose(X, [axis] + range(0, axis) + range(axis+1, X.ndim))
         axis_ = 0
     mM = np.percentile(X, percentile, axis=axis_)
-    indices_min = np.where(X < np.tile(mM[0], [X.shape[0], 1, 1]))
-    indices_max = np.where(X > np.tile(mM[1], [X.shape[0], 1, 1]))
+    indices_min = np.where((X - mM[0][np.newaxis, ...]) < 0)
+    indices_max = np.where((X - mM[1][np.newaxis, ...]) > 0)
     X[indices_min] = np.nan
     X[indices_max] = np.nan
     m = np.nanmean(X, axis=axis_)