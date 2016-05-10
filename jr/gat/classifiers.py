7b47f2e22c926c58be186e92f47abe51d0e14f96
kingjr <jeanremi.king+github@gmail.com>
2016-05-10 09:18:47 -0400

    FIX: default clf for angular classifier

--- jr/gat/classifiers.py
+++ jr/gat/classifiers.py
@@ -280,7 +280,7 @@ class AngularClassifier(BaseEstimator):
 
     def __init__(self, clf=None, bins=None, predict_method='predict_proba'):
         if clf is None:
-            clf = LinearSVC()
+            clf = LinearSVC(probability=predict_method == 'predict_proba')
         self.clf = clf
         if bins is None:
             n_bins = 10
