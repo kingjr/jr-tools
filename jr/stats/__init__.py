8e98370cf0b31c8972c60f11ce261fdf36413886
kingjr <jeanremi.king+github@gmail.com>
2016-06-10 13:16:03 -0400

    ENH: fast wilcoxon

--- jr/stats/__init__.py
+++ jr/stats/__init__.py
@@ -2,7 +2,7 @@ from .base import (corr_linear_circular, repeated_corr, fast_mannwhitneyu,
                    repeated_spearman, corr_circular, robust_mean, dPrime,
                    mannwhitneyu, corr_circular_linear, nested_analysis,
                    median_abs_deviation, cross_correlation_fft, align_signals,
-                   cross_correlation)
+                   cross_correlation, fast_wilcoxon)
 from .gat_stats import parallel_stats, stats_tfce
 from .circ import (circ_hist, circ_tuning, circ_double_tuning, circ_diff,
                    circ_mean, circ_cummean, circ_std, circ_digitize,
