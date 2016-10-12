0dbfcd5f59b5d09431cead3e1d456e121bcd86e4
kingjr <jeanremi.king@gmail.com>
2016-10-12 19:29:17 -0400

    update to mne .13

--- jr/gat/__init__.py
+++ jr/gat/__init__.py
@@ -13,7 +13,7 @@ from .scorers import (scorer_spearman, scorer_auc, prob_accuracy, scorer_angle,
                       scorer_corr)
 from .graphs import plot_graph, annotate_graph, animate_graph
 from .preprocessing import Averager, MeanFeatures, DigitizedTransformer
-from .transformers import (TimeFreqDecomposer, TimePadder, TimeSelector,
+from .transformers import (TimePadder, TimeSelector,
                            TimeFreqSelector, EpochsBaseliner,
                            TimeFreqBaseliner, MyXDawn,
                            SpatialFilter, Reshaper,
